1eb84eb0d7fdc3ebd997bd4677dca1c9814486f3
robertrendler <72698016+robertrendler@users.noreply.github.com>
2022-03-13 19:30:54 +1100

    Add files via upload

---
+++ JK.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+my $JK = 'H:\Games\Steam\steamapps\common\Jump King';
+
+my @mods = rD('Content', 'mod');
+
+my %mods;
+for my $mod (@mods) {
+    my $modF = rD("Content/$mod", '\.mod$');
+
+    open MODF, "$JK/Content/$mod/$modF" or die "Couldn't open file: $!\n";
+    chomp ( my $modN = <MODF> );
+    close MODF;
+
+    $modF =~ s/\.mod$//;
+    $mods{$mod} = { name => $modN, modf => $modF };
+}
+
+print "JUMP KING MODS MANAGER.\n\n";
+if ($mods{mods}) {
+    print "Mod Enabled: $mods{mods}{name}\n\n";
+}
+
+print "Mods Avaiable: \n";
+
+my $i = 0;
+my @options;
+
+for (sort keys %mods) {
+    unless (/^mods$/) {
+        ++$i;
+        print "[ $i ] $mods{$_}{name}\n";
+        push @options, $mods{$_}{modf};
+    }
+}
+if ($mods{mods}) {
+    print "[ 3 ] DISABLE MOD.\n";
+    push @options, $mods{mods}{modf};
+}
+
+print ": ";
+chomp ( my $option = <STDIN> );
+if ($option =~ /^\d+$/ && $option > 0 && $options[--$option]) {
+    my $m = $options[$option];
+    if ($option == $#options) {
+        rename "$JK/Content/mods", "$JK/Content/mods.$m" or die $!;
+        print "DISABLED!\n";
+    } else {
+        if ($mods{mods}) {
+            rename "$JK/Content/mods", "$JK/Content/mods.$mods{mods}{modf}" or die $!;
+        }
+        
+        rename "$JK/Content/mods.$m", "$JK/Content/mods" or die $!;
+        print "Mod Enabled.\n" ;
+    }
+}
+
+
+sub rD {
+    my ($d, $t) = @_;
+    my @d;
+    opendir JK, "$JK/$d" or die "Couldn't open directory: $!\n";
+    while (readdir JK) {
+        push @d, $_ if /$t/;
+    }
+    closedir JK;
+    return $#d > 0 ? @d : shift @d;
+}
